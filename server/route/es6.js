d7526f90118a3135c42ab931f9b9f192907021b7
sky <hubcarl@126.com>
2016-05-13 18:45:02 +0800

    es6 学习

--- server/route/es6.js
+++ server/route/es6.js
@@ -2,7 +2,7 @@
 'use strict';
 
 /** 
- * es6 
+ * es6 http://es6.ruanyifeng.com/#docs/async
  * Description：es6使用
  * Created by sky on 16/5/12
  * Copyright (c) 2016 sky All Rights Reserved
@@ -45,6 +45,59 @@ router.get('/es6', function* () {
 
     const copy1 = Object.assign({__proto__: source1.__proto__}, source1);
     const copy2 = Object.assign({}, source1);
+
+    //Proxy可以理解成，在目标对象之前架设一层“拦截”，外界对该对象的访问，都必须先通过这层拦截，因此提供了一种机制，可以对外界的访问进行过滤和改写
+    //代码对一个空对象架设了一层拦截，重定义了属性的读取（get）和设置（set）行为。这里暂时先不解释具体的语法,上面代码说明，Proxy实际上重载（overload）了点运算符，即用自己的定义覆盖了语言的原始定义。
+
+    var obj = new Proxy({}, {
+        get: function (target, key, receiver) {
+            console.log(`getting ${key}!`);
+            return Reflect.get(target, key, receiver);
+        },
+        set: function (target, key, value, receiver) {
+            console.log(`setting ${key}!`);
+            return Reflect.set(target, key, value, receiver);
+        }
+    });
+
+    for (let c of 'foo') {
+        console.log(c);
+    }
+
+    Array.from([1,2,3], x => x * x);
+    Array.from([1,2,3]).map(x => x * x);
+    Array.of(3, 11, 8) // [3,11,8]
+
+    for (let index of ['a', 'b'].keys()) {
+        console.log(index);
+    }
+
+    for (let elem of ['a', 'b'].values()) {
+        console.log(elem);
+    }
+
+
+    for (let [index, elem] of ['a', 'b'].entries()) {
+        console.log(index, elem);
+    }
+
+    [1, 2, 3].includes(2);     // true
+    [1, 2, 3].includes(4);     // false
+    [1, 2, NaN].includes(NaN); // true
+
+    //includes()：返回布尔值，表示是否找到了参数字符串。
+    //startsWith()：返回布尔值，表示参数字符串是否在源字符串的头部。
+    //endsWith()：返回布尔值，表示参数字符串是否在源字符串的尾部。
+
+    var s = 'Hello world!';
+    s.startsWith('Hello') // true
+    s.endsWith('!') // true
+    s.includes('o') // true
+
+    //这三个方法都支持第二个参数，表示开始搜索的位置。
+    s.startsWith('world', 6) // true
+    s.endsWith('Hello', 5) // true
+    s.includes('Hello', 6) // false
 
     //在 ECMAScript 6 中， {x, y} 是 {x: x, y: y} 的一种缩写形式。
     yield this.render('es6',{title:'es6学习', data:{
