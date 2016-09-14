1db7562f61bbc5235b4351b7c8292d264754e58c
sky <cl98914@alibaba-inc.com>
2016-09-14 18:18:32 +0800

    support react server render

--- fis-conf.js
+++ fis-conf.js
@@ -11,16 +11,16 @@ fis.set('server.entry', 'app.js');
 
 fis.log.level = fis.log.L_INFO;
 
-fis.hook('commonjs',{});
-
 //设置客户端require component组件查找目录
 fis.set('component.dir','client/public/vendor');
 
-//fis.on('compile:end', function(file){
-//    if(file.realpath.indexOf('hello')>-1){
-//        console.log(file);
-//    }
-//});
+fis.hook('commonjs',{
+    //paths: {
+    //    'react': 'client/public/vendor/react',
+    //    'react-dom': 'client/public/vendor/react-dom'
+    //}
+});
+
 
 //fis.unhook('components'); // fis3 中预设的是 fis-components，这里不需要，所以先关了。
 //fis.hook('node_modules'); // 使用 fis3-hook-node_modules 插件。
@@ -52,7 +52,7 @@ fis.match('/{client/public,client/views}/(**){.jsx,:jsx}', {
     release: '/client/public/$1'
 });
 
-// 编译所有后缀为 jsx 的文件为 js
+//编译所有后缀为 jsx 的文件为 js
 fis.match('/client/views/widget/{hello/*,pager/*}{.jsx,:jsx}', {
     useRequireReplace:false
 });
@@ -129,9 +129,3 @@ fis.media('dev').match('*.{js,css,png}', {
     useSprite: false,
     optimizer: null
 })
-
-//fis-preprocessor-components 给components 添加短路径功能
-fis.config.set('settings.preprocessor.components.paths', {
-    'react': 'common:client/public/vendor/react/react.js',
-    'react-dom': 'common:client/public/vendor/react-dom/react-dom.js'
-});
